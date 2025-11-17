be1751e3ca70b1b688e481d189c0321c45cf7aa0
Andrew Sigei <andrew.sigei@student.moringaschool.com>
2025-11-18 00:35:20 +0300

    add initial class structure with properties, setters, and class variables

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,12 +1,75 @@
 class Article:
-    def __init__(self, author, magazine, title):
+
+    # 1. This is the CLASS VARIABLE.
+    # It will hold all Article instances.
+    all = []
+
+    def __init__(self, author, magazine, title):  # Refers to the new article
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+
+        # 2. 'self' is the new Article instance.
+        # We add it to the shared 'all' list.
+        Article.all.append(self)
+
+    # Now future relationship methods can look through Article.all to answer questions like:
+    # "Which articles belong to this author?"
+    # "Which articles belong to this magazine?"
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        # Immutable: use the hasattr check
+        if not hasattr(self, "_title"):
+            if isinstance(title, str) and 5 <= len(title) <= 50: # Validation of data (between 5 and 50 characters)
+                self._title = title
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, new_author):
+        # Mutable: just check the type
+        if isinstance(new_author, Author):
+            self._author = new_author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, new_magazine):
+        # Mutable: just check the type
+        if isinstance(new_magazine, Magazine):
+            self._magazine = new_magazine
+
+
 class Author:
     def __init__(self, name):
+        # Calls @name.setter
         self.name = name
+
+    @property
+    def name(self):
+        # This is the getter
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        # Check for immutability
+        if not hasattr(self, "_name"):
+            # Checks validation rules:
+            if isinstance(name, str) and len(name) > 0:
+                # If valid set the private variable
+                self._name = name
+            else:
+                # This is for bonus round
+                pass
 
     def articles(self):
         pass
@@ -20,10 +83,40 @@ class Author:
     def topic_areas(self):
         pass
 
+
 class Magazine:
-    def __init__(self, name, category):
+
+    # 1. This is the CLASS VARIABLE for Magazine.
+    # It will hold all Magazine instances.
+    all = []
+
+    def __init__(self, name, category): # Initialisises a new object with all its required data 
         self.name = name
         self.category = category
+
+        # 2. 'self' is the new Magazine instance.
+        # We add it to the shared 'all' list.
+        Magazine.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        # Mutable: Just Validate the new value
+        if isinstance(new_name, str) and 2 <= len(new_name) <= 16:
+            self._name = new_name
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, new_category):
+        # Mutable: just validate the new value
+        if isinstance(new_category, str) and len(new_category) > 0:
+            self._category = new_category
 
     def articles(self):
         pass
@@ -36,5 +129,3 @@ class Magazine:
 
     def contributing_authors(self):
         pass
-
-    
--- lib/debug.py
+++ lib/debug.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import ipdb;
+import ipdb
 
 from classes.many_to_many import Article
 from classes.many_to_many import Author
@@ -8,6 +8,17 @@ from classes.many_to_many import Magazine
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
+    # --- Add test code HERE ---
+    Article.all = []  # Clear the lists just in case
+    Magazine.all = []
+
+    a1 = Author("J.K. Rowling")
+    m1 = Magazine("The New Yorker", "Literature")
+
+    # This line will run the __init__ methods
+    art1 = Article(a1, m1, "A New Story")
+    art2 = Article(a1, m1, "Another Story")
+    # --- End test code ---
 
     # don't remove this line, it's for debugging!
     ipdb.set_trace()
