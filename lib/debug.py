b29809ab5ca8c302f52acca700724ea8fdf5fe39
Andrew Sigei <andrew.sigei@student.moringaschool.com>
2025-11-18 00:42:05 +0300

    Update debug.py tests

--- lib/debug.py
+++ lib/debug.py
@@ -8,17 +8,18 @@ from classes.many_to_many import Magazine
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
-    # --- Add test code HERE ---
+    #  Add test code HERE 
     Article.all = []  # Clear the lists just in case
     Magazine.all = []
 
     a1 = Author("J.K. Rowling")
     m1 = Magazine("The New Yorker", "Literature")
 
-    # This line will run the __init__ methods
-    art1 = Article(a1, m1, "A New Story")
-    art2 = Article(a1, m1, "Another Story")
-    # --- End test code ---
+## Intentionally give bad inputs / push the boundaries - All tests should fail
+    a_invalid = Author("") # Author must be non empty  
+    m_invalid = Magazine("OK", "News") # OK is too short
+    art_valid = Article(a1, m1, "Hi") # Hi is too short
 
     # don't remove this line, it's for debugging!
     ipdb.set_trace()
+    
