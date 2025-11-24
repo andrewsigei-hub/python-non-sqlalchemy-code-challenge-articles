5ad82a75c1e7e785a4f5dce62777c120ea2e8854
Andrew Sigei <andrew.sigei@student.moringaschool.com>
2025-11-25 00:29:05 +0300

    added getters and setters for Article class

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,9 +1,43 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        if isinstance(title, str) and 5 <= len(title) <= 50:
+            self._title = title
+        else:
+            raise ValueError("Title invalid")
+
+    @property
+    def author(self, author):
+        return self._author
+
+    @author.setter
+    def author(self, author):
+        pass
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, magazine):
+        pass
+
+    
+
+
 class Author:
     def __init__(self, name):
         self.name = name
@@ -19,6 +53,7 @@ class Author:
 
     def topic_areas(self):
         pass
+
 
 class Magazine:
     def __init__(self, name, category):
